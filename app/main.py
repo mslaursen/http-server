3e8066307ce8a47753095aff877327bf1df108eb
martinlaursen97 <martin_laursen21@hotmail.com>
2024-02-16 00:13:47 +0100

    404

--- app/main.py
+++ app/main.py
@@ -8,13 +8,33 @@ listen_socket.listen(1)
 print("Serving HTTP on port %s ..." % PORT)
 
 
+def parse_request(request: bytes) -> str:
+
+    text = request.decode()
+    path = text.split("\r\n")[0].split()[1]
+
+    response_status = ""
+
+    match path:
+        case "/":
+            response_status = "200 OK"
+        case _:
+            response_status = "404 Not Found"
+
+    return response_status
+
+
 while True:
     client_connection, client_address = listen_socket.accept()
 
     request = client_connection.recv(1024)
-    print(request.decode("utf-8"))
 
-    http_response = "HTTP/1.1 200 OK\r\n\r\n"
+    text = request.decode()
+    path = text.split("\r\n")[0].split()[1]
+
+    response_status = parse_request(request)
+
+    http_response = f"HTTP/1.1 {response_status}\r\n\r\n"
 
     client_connection.sendall(http_response.encode("utf-8"))
     client_connection.close()
