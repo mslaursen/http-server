b64403ea3a6fe97af419e8e088496fb3eba51444
martinlaursen97 <martin_laursen21@hotmail.com>
2024-02-16 00:46:58 +0100

    cleanup

--- app/main.py
+++ app/main.py
@@ -6,22 +6,30 @@ listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.bind((HOST, PORT))
 listen_socket.listen(1)
 
+HTTP_VERSION = "HTTP/1.1"
+CRLF = "\r\n"
+HTTP_LB = CRLF * 2
+
+
 print(f"Serving HTTP on port {PORT}")
 
 
-def parse_request(request: bytes) -> str:
+def parse_request_line(request: bytes) -> tuple[str, str, str]:
+    request_line = request.decode().split(CRLF)[0]
+    partitions = request_line.split()
+    if len(partitions) != 3:
+        raise ValueError(request_line)
+    method, path, version = partitions
+    return method, path, version
 
-    text = request.decode()
-    path = text.split("\r\n")[0].split()[1]
 
+def get_response_status(path: str) -> str:
     response_status = ""
-
     match path:
         case "/":
             response_status = "200 OK"
         case _:
             response_status = "404 Not Found"
-
     return response_status
 
 
@@ -31,11 +39,11 @@ while True:
     request = client_connection.recv(1024)
 
     text = request.decode()
-    path = text.split("\r\n")[0].split()[1]
 
-    response_status = parse_request(request)
+    _, path, _ = parse_request_line(request)
+    response_status = get_response_status(path)
 
-    http_response = f"HTTP/1.1 {response_status}\r\n\r\n"
+    http_response = f"{HTTP_VERSION} {response_status}{HTTP_LB}"
 
     client_connection.sendall(http_response.encode("utf-8"))
     client_connection.close()
