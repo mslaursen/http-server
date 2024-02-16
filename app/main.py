125e41ce342a7f3c598fde35ffdea720231f8ddf
mslaursen <martin_laursen21@hotmail.com>
2024-02-16 17:41:46 +0100

    .

--- app/main.py
+++ app/main.py
@@ -1,37 +1,39 @@
 import socket
 
 
+HOST, PORT = "", 4221
+listen_socket = socket.create_server((HOST, PORT), reuse_port=True)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+
+HTTP_VERSION = "HTTP/1.1"
+CRLF = "\r\n"
+HTTP_LB = CRLF * 2
+BUFFER_SIZE = 1024
+
+print(f"Serving HTTP on port {PORT}")
+
+
+def parse_request_line(request: bytes) -> tuple[str, str, str]:
+    request_line = request.decode().split(CRLF)[0]
+    partitions = request_line.split()
+    if len(partitions) != 3:
+        raise ValueError(request_line)
+    method, path, version = partitions
+    return method, path, version
+
+
+def get_response_status(path: str) -> str:
+    response_status = ""
+    match path:
+        case "/":
+            response_status = "200 OK"
+        case _:
+            response_status = "404 Not Found"
+    return response_status
+
+
 def main() -> None:
-
-    HOST, PORT = "", 4221
-    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    listen_socket.bind((HOST, PORT))
-    listen_socket.listen(1)
-
-    HTTP_VERSION = "HTTP/1.1"
-    CRLF = "\r\n"
-    HTTP_LB = CRLF * 2
-    BUFFER_SIZE = 1024
-
-    print(f"Serving HTTP on port {PORT}")
-
-    def parse_request_line(request: bytes) -> tuple[str, str, str]:
-        request_line = request.decode().split(CRLF)[0]
-        partitions = request_line.split()
-        if len(partitions) != 3:
-            raise ValueError(request_line)
-        method, path, version = partitions
-        return method, path, version
-
-    def get_response_status(path: str) -> str:
-        response_status = ""
-        match path:
-            case "/":
-                response_status = "200 OK"
-            case _:
-                response_status = "404 Not Found"
-        return response_status
-
     while True:
         client_connection, _ = listen_socket.accept()
 
