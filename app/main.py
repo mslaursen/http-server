232af25914185008ea3cf747d46804affa39f9a7
martinlaursen97 <martin_laursen21@hotmail.com>
2024-02-16 10:18:48 +0100

    ...

--- app/main.py
+++ app/main.py
@@ -1,50 +1,31 @@
+# Uncomment this to pass the first stage
 import socket
 
 
-HOST, PORT = "", 4221
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+    # Uncomment this to pass the first stage
+    #
+    server = socket.create_server(("localhost", 4221), reuse_port=True)
+    server.listen()
+    # server_socket.accept() # wait for client
+    while True:
+        server_socket, config = server.accept()
+        data = server_socket.recv(1024)
+        if not data:
+            break
+        server_socket.send("HTTP/1.1 200 OK\r\n\r\n".encode("ascii"))
+        stringData = data.decode("ascii")
+        lines = stringData.split("\r\n")
+        verb, path, protocol = lines[0].split(" ")
+        if verb == "GET":
+            if path == "/":
+                server_socket.send(
+                    "HTTP/1.1 200 OK\r\n\r\nHello, World!".encode("ascii")
+                )
+            else:
 
-HTTP_VERSION = "HTTP/1.1"
-CRLF = "\r\n"
-HTTP_LB = CRLF * 2
-
-
-print(f"Serving HTTP on port {PORT}")
-
-
-def parse_request_line(request: bytes) -> tuple[str, str, str]:
-    request_line = request.decode().split(CRLF)[0]
-    partitions = request_line.split()
-    if len(partitions) != 3:
-        raise ValueError(request_line)
-    method, path, version = partitions
-    return method, path, version
-
-
-def get_response_status(path: str) -> str:
-    response_status = ""
-    match path:
-        case "/":
-            response_status = "200 OK"
-        case _:
-            response_status = "404 Not Found"
-    return response_status
-
-
-while True:
-    ##
-    client_connection, client_address = listen_socket.accept()
-
-    request = client_connection.recv(1024)
-
-    text = request.decode()
-
-    _, path, _ = parse_request_line(request)
-    response_status = get_response_status(path)
-
-    http_response = f"{HTTP_VERSION} {response_status}{HTTP_LB}"
-
-    client_connection.sendall(http_response.encode("utf-8"))
-    client_connection.close()
+                server_socket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode("ascii"))
+        # server_socket.send("HTTP/1.1 200 OK\r\n\r\n".encode("ascii"))
+        server_socket.close()
