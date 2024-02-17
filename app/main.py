69daec8da75841f1b5978944318a8de018c13c1d
mslaursen <martin_laursen21@hotmail.com>
2024-02-17 15:00:50 +0100

    s8

--- app/main.py
+++ app/main.py
@@ -9,25 +9,32 @@ HOST, PORT = "", 4221
 BUFFER_SIZE = 1024
 HTTP_VERSION = "HTTP/1.1"
 CRLF = "\r\n"
-HTTP_LB = CRLF * 2
+HTTP_HEADER_END = CRLF * 2
 
 
 class HTTPRequest:
     def __init__(self, request_text: str):
-        self.method, self.path, self.version, self.headers = self._parse_request(
-            request_text
+        self.method, self.path, self.version, self.body, self.headers = (
+            self._parse_request(request_text)
         )
 
     @staticmethod
-    def _parse_request(request_text: str) -> tuple[str, str, str, dict[str, Any]]:
-        lines = request_text.split(CRLF)
+    def _parse_request(
+        request_text: str,
+    ) -> tuple[str, str, str, str | None, dict[str, Any]]:
+        body_split = request_text.split(HTTP_HEADER_END)
+        body = None
+        if len(body_split) > 1:
+            body = body_split[1]
+
+        lines = body_split[0].split(CRLF)
         method, path, version = lines[0].split(" ")
         headers = {
             line.split(": ")[0]: line.split(": ")[1]
             for line in lines[1:]
             if ": " in line
         }
-        return method, path, version, headers
+        return method, path, version, body, headers
 
 
 class HTTPResponse:
@@ -45,6 +52,12 @@ class HTTPResponse:
         self._process_request()
 
     def _process_request(self) -> None:
+        if self.request.method == "GET":
+            self._handle_get_request()
+        elif self.request.method == "POST":
+            self._handle_post_request()
+
+    def _handle_get_request(self) -> None:
         path_header = self.request.path[1:].title()
         if self.request.path == "/":
             self.body = "Home page"
@@ -74,15 +87,32 @@ class HTTPResponse:
         if self.body is not None:
             self.headers["Content-Length"] = str(len(self.body))
 
+    def _handle_post_request(self) -> None:
+        files_dir: Path = Path(__file__).parent.parent / self.parsed_args.directory
+        file_path = files_dir / self.request.path[7:]
+
+        if self.request.path.startswith("/files/"):
+            if self.request.body:
+                with open(file_path, "w") as f:
+                    f.write(self.request.body)
+
+                self._set_response_created()
+
     def _set_response_not_found(self) -> None:
         self.status_code = "404"
         self.status_text = "Not Found"
         self.body = "Page not found"
 
+    def _set_response_created(self) -> None:
+        self.status_code = "201"
+        self.status_code = "Created"
+
     def build_response(self) -> bytes:
         status_line = f"{HTTP_VERSION} {self.status_code} {self.status_text}{CRLF}"
         headers = CRLF.join([f"{key}: {value}" for key, value in self.headers.items()])
-        return f'{status_line}{headers}{HTTP_LB}{self.body or ""}'.encode("utf-8")
+        return f'{status_line}{headers}{HTTP_HEADER_END}{self.body or ""}'.encode(
+            "utf-8"
+        )
 
 
 def handle_client_connection(
@@ -91,10 +121,17 @@ def handle_client_connection(
     try:
         request_data = client_connection.recv(BUFFER_SIZE).decode("utf-8")
         request = HTTPRequest(request_data)
+
+        print(request_data)
         response = HTTPResponse(request, parsed_args)
         client_connection.sendall(response.build_response())
+
+        raise Exception()
     except Exception as e:
         print(f"Error: {e}")
+        import sys
+
+        sys.exit(1)
     finally:
         client_connection.close()
 
